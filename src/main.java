fc637eb4b4dac34c95f7772858d15352f7f6323d
Tawanda Chiteshe <dickenstawanda@gmail.com>
2019-10-13 00:49:43 +0200

    progress quite .... improving with a dataseet Class

--- src/main.java
+++ src/main.java
@@ -1,4 +1,8 @@
 
+/*
+ * Copyright (c) 2019. Author Tawanda Chiteshe
+ */
+
 import util.*;
 import util.NN.ActivationFunctions;
 import util.NN.NeuralNetwork;
@@ -10,19 +14,16 @@ public class main {
         matrix m = new matrix(2,3);
         m.randomize();
         double[] arry = matrix.toArray(m);
-        double dataSet[] = new double[]{1};
-        double e[] = new double[]{1,0};
-       NeuralNetwork nn = new NeuralNetwork(2,3,1);
+        double dataSet[] = new double[]{0};
+        double e[] = new double[]{1,1};
+       NeuralNetwork nn = new NeuralNetwork(2,2,1);
 
-        for (int i = 0; i < 100; i++) {
-
-            matrix predict =  nn.predict(e);
-            double[] pedict = matrix.toArray(predict);
-            nn.predict(e);
+        for (int i = 0; i < 5000; i++) {
             nn.train(e, dataSet);
-
-            System.out.println( Arrays.deepToString(predict.getData()));
         }
+
+        System.out.println( Arrays.deepToString(nn.predict(e).getData()));
+
         }
 
 }
